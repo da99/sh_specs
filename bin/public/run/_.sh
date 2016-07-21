6b10d4a91b25e04220dba67ff60f6a58ca2f82ec
da99 <i-hate-spam-1234567@mailinator.com>
2016-07-21 03:56:05 -0500

    Dev. checkpoint: Touchup.

--- bin/public/run/_.sh
+++ bin/public/run/_.sh
@@ -1,5 +1,7 @@
 
 
+source "$THIS_DIR/bin/private/desc.sh"
+
 # === {{CMD}}                  # Run all specs in bin/
 # === {{CMD}}  My_Perl_RegExp
 run() {
@@ -9,7 +11,6 @@ run() {
   local +x SKIPS=""
   local +x IFS=$'\n'
 
-
   if [[ -z "$@" ]]; then
     local +x SEARCH="/bin/"
   else
@@ -17,8 +18,18 @@ run() {
   fi
 
   for FILE in $(find -L "$PWD/bin/" -maxdepth 3 -mindepth 3 -type f -name "*.sh" -and -not -name "_.*" | grep -P "$SEARCH" | sort); do
+    mksh_setup BOLD -n "{{$(basename "$(dirname "$FILE")" )}}  "
+    mksh_setup BOLD "({{$(basename "$(dirname "$(dirname "$FILE")" )" )}})"
     $THIS_DIR/bin/private/run-file "$THIS_DIR" "$FILE"
+    FOUND="yes"
+    echo ""
   done
+
+  if [[ -z "$FOUND" ]]; then
+    mksh_setup RED "!!! {{No specs found}}."
+    exit 1
+  fi
+
 
 } # === end func run
 
