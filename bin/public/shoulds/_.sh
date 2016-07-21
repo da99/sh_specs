a06e9994282fec5d895ee106ee96d12ba9f4dc5c
da99 <i-hate-spam-1234567@mailinator.com>
2016-07-21 02:21:52 -0500

    Moved dirs to match bin/public layout.

--- bin/public/shoulds/_.sh
+++ bin/public/shoulds/_.sh
@@ -1,5 +1,5 @@
 
 # === {{CMD}}
 shoulds () {
-  find "$THIS_DIR/bin/public" -type f -name "should-*.sh" | sort | xargs -I FILE basename FILE .sh
+  find "$THIS_DIR/bin/shoulds" -type f -name "should-*.sh" | sort | xargs -I FILE basename FILE .sh
 } # === end function
