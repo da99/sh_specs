ef7b40d3b74a7d05e1ff617162e1622c2b0722e9
da99 <da@bdrm>
2018-05-26 18:47:24 -0500

    Fixed: local PATH makes it null in child processes.

--- bin/public/run/_.sh
+++ bin/public/run/_.sh
@@ -5,7 +5,7 @@
 # ===   sh/*/specs
 # ===   sh/specs
 run () {
-  local +x PATH="$PATH:$THIS_DIR/../sh_color/bin"
+  PATH="$PATH:$THIS_DIR/../sh_color/bin"
   find-specs | while IFS=$'\n' read -r FILE ; do
     { ./"$FILE" &&  sh_color GREEN "=== {{PASSED}}: BOLD{{$FILE}}" ; } || {
       sh_color RED "!!! {{FAILED}}: $? BOLD{{$FILE}}"
